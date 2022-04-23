05c0c2dda83690eadd5a7110c4bad82ecb45ca19
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-04-23 14:32:00 +0100

    Added  Programming Basics course solutions.

---
+++ Programming-Basics-C#/Exercises/petshop/petshop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace petshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numdogs = int.Parse(Console.ReadLine());
+            int numothersanimals = int.Parse(Console.ReadLine());
+            double price = (numdogs*2.50) +(numothersanimals*4) ;
+            Console.WriteLine($"{price} lv.");
+        }
+    }
+}
