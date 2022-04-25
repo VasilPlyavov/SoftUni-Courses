d59b155dfb3aeb1ebc7066ded7ad941500665883
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-04-25 14:31:34 +0100

    Added Exercises

---
+++ Programming-Basics-C#/Exercises/01.First steps in Programming/Fish Tank/Fish Tank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l = int.Parse(Console.ReadLine());
+            int w = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+
+            double volume = l * w * h;
+            double totalLitres = volume * 0.001;
+            double totalPercentage = percentage * 0.01;
+            double litresNeeded = totalLitres * (1 - totalPercentage);
+
+            Console.WriteLine(litresNeeded);
+        }
+    }
+}
---
+++ Programming-Basics-C#/Exercises/01.First steps in Programming/Fruit Market/Fruit Market/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fruit_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double strawberryPrice = double.Parse(Console.ReadLine());
+            double bananaskg = double.Parse(Console.ReadLine());
+            double orangekg = double.Parse(Console.ReadLine());
+            double raspberrykg = double.Parse(Console.ReadLine());
+            double strawberrykg = double.Parse(Console.ReadLine());
+
+            double raspberryPrice = strawberryPrice / 2;
+            double orangePrice = raspberryPrice - (raspberryPrice * 0.4);
+            double bananaPrice = raspberryPrice - (raspberryPrice * 0.8);
+            double sumraspberry = raspberrykg * raspberryPrice;
+            double sumorange = orangekg * orangePrice;
+            double sumbanana = bananaskg * bananaPrice;
+            double sumstrawberry = strawberrykg * strawberryPrice;
+            double totalPrice = sumraspberry + sumorange + sumbanana + sumstrawberry;
+
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+    }
+}
