1db9c70de958997b4dedc2d3700282a7c5507bd4
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-04-26 21:13:17 +0100

    Added exercises

---
+++ Programming-Basics-C#/Exercises/01.First steps in Programming/Vacation books list/Vacation books list/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Vacation_books_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pages = int.Parse(Console.ReadLine());
+            double pagesforOneday = double.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            double totaltime = pages / pagesforOneday;
+            double hours = totaltime / days;
+            Console.WriteLine(hours);
+        }
+    }
+}
