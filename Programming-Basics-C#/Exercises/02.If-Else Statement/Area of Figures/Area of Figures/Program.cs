d02a2063838139fb16cc89ff39f1e5e460e993ae
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-04-27 20:59:34 +0100

    Added Exercises

---
+++ Programming-Basics-C#/Exercises/02.If-Else Statement/Area of Figures/Area of Figures/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = (Console.ReadLine());
+            if (type == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double result = side * side;
+                Console.WriteLine($"{result:f3}");
+            }
+            else if (type == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideb = double.Parse(Console.ReadLine());
+                double result = sideA * sideb;
+                Console.WriteLine($"{result:f3}");
+            }
+            else if (type == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                double result = Math.PI * r * r;
+                Console.WriteLine($"{result:f3}");
+            }
+            else if (type == "triangle")
+            {
+                double l = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                double result = (l * h)/2;
+                Console.WriteLine($"{result:f3}");
+            }
+        }
+            
+        
+    }
+}
---
+++ Programming-Basics-C#/Exercises/02.If-Else Statement/Excellent Result/Excellent Result/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Excellent_Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grades = double.Parse(Console.ReadLine());
+            if (grades>=5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+            
+        }
+    }
+}
