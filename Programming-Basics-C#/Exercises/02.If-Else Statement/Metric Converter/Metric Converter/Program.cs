d55f192fd9fbf1c4a93b920e89eb00e5a5e6296b
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-05-02 12:43:41 +0100

    Exercises and Labs

---
+++ Programming-Basics-C#/Exercises/02.If-Else Statement/Metric Converter/Metric Converter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Metric_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            string inputMetric = (Console.ReadLine());
+            string outputMetric = (Console.ReadLine());
+
+            if (inputMetric == "mm")
+            {
+                number = number / 1000;
+            }
+            else if(inputMetric == "cm")
+            {
+                number = number / 100;
+            }
+            else 
+            {
+                number = number;
+            }
+
+            if(outputMetric=="m")
+            {
+                Console.WriteLine($"{number:F3}");
+            }
+            else if(outputMetric=="cm")
+            {
+                Console.WriteLine($"{(number*100):F3}");
+            }
+            else if(outputMetric =="mm")
+            {
+                Console.WriteLine($"{(number * 1000):F3}");
+            }
+           
+        }
+    }
+}
---
+++ Programming-Basics-C#/Exercises/02.If-Else Statement/Scholarship/Scholarship/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Scholarship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double income = double.Parse(Console.ReadLine());
+            double averagesucces = double.Parse(Console.ReadLine());
+            double minsalary = double.Parse(Console.ReadLine());
+            double excellent = (averagesucces * 25);
+            double social = (minsalary * 0.35);
+            double ex = Math.Floor(excellent);
+            double so = Math.Floor(social);
+            if (averagesucces >= 5.50)
+            {
+                if (social > excellent && income < minsalary)
+                {
+                    Console.WriteLine($"You get a Social scholarship {so} BGN");
+                }
+                else
+                {
+                    Console.WriteLine($"You get a scholarship for excellent results {ex} BGN");
+                }
+            }
+            else if(averagesucces >= 4.50 && averagesucces <5.50)
+                {
+                if (income < minsalary)
+                {
+                    Console.WriteLine($"You get a scholarship {so} BGN");
+                }
+                else
+                {
+                    Console.WriteLine($"You cannot get a scholarship!");
+                }
+
+            }
+            else if(averagesucces<=4.50)
+            {
+                Console.WriteLine($"You cannot get a scholarship!");
+            }
+        }
+    }
+}
