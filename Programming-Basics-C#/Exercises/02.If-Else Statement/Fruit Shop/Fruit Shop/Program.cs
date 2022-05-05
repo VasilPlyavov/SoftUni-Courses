26574c7ce7b7a55f95440eeb180d4f024ab4e303
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-05-05 14:40:07 +0100

    More exercises and homework

---
+++ Programming-Basics-C#/Exercises/02.If-Else Statement/Fruit Shop/Fruit Shop/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Fruit_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string day = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            double price = 0;
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                    switch (fruit)
+                    {
+                        case "banana": Console.WriteLine($"{(price = quantity * 2.50):f2}"); break;
+                        case "apple": Console.WriteLine($"{(price = quantity * 1.20):f2}"); break;
+                        case "orange": Console.WriteLine($"{(price = quantity * 0.85):f2}"); break;
+                        case "grapefruit": Console.WriteLine($"{(price = quantity * 1.45):f2}"); break;
+                        case "kiwi": Console.WriteLine($"{(price = quantity * 2.70):f2}"); break;
+                        case "pineapple": Console.WriteLine($"{(price = quantity * 5.50):f2}"); break;
+                        case "grapes":Console.WriteLine($"{(price = quantity * 3.85):f2}"); break;
+                            
+                        default:
+                            Console.WriteLine("error");break;
+                    }
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    switch (fruit)
+                    {
+                        case "banana": Console.WriteLine($"{(price = quantity * 2.70):f2}"); break;
+                        case "apple": Console.WriteLine($"{(price = quantity * 1.25):f2}"); break;
+                        case "orange": Console.WriteLine($"{(price = quantity * 0.90):f2}"); break;
+                        case "grapefruit": Console.WriteLine($"{(price = quantity * 1.60):f2}"); break;
+                        case "kiwi": Console.WriteLine($"{(price = quantity * 3.00):f2}"); break;
+                        case "pineapple": Console.WriteLine($"{(price = quantity * 5.60):f2}"); break;
+                        case "grapes":Console.WriteLine($"{(price = quantity * 4.20):f2}"); break;
+                            
+                    }
+                    break;
+                default:
+                    Console.WriteLine("error"); break;
+            }
+            
+            
+        }
+        
+    }
+}
