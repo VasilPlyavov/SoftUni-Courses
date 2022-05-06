b7a1066b102c5106017fa314cb309c4ef3d53b5f
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-05-06 14:49:22 +0100

    Added exercises

---
+++ Programming-Basics-C#/Exercises/02.If-Else Statement/Invalid Number/Invalid Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Invalid_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if((num >= 100 && num <= 200) || num == 0)
+            {
+              
+            }
+            else if(num<=100 || num >= 200)
+            {
+                Console.WriteLine("invalid");
+            }
+          
+           
+           
+        }
+    }
+}
