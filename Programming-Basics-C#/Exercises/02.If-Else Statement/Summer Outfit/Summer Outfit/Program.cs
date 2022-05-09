49cea24b4c8494acffe2c16a602742708996be6b
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-05-09 14:46:59 +0100

    Added more Exercises

---
+++ Programming-Basics-C#/Exercises/02.If-Else Statement/Summer Outfit/Summer Outfit/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Summer_Outfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degrees = int.Parse(Console.ReadLine());
+            string timeofday = Console.ReadLine();
+            string outfit= "";
+            string shoes = "";
+
+            if(degrees>=10 && degrees<=18)
+            {
+                if(timeofday == "Morning")
+                {
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+                else if(timeofday =="Afternoon" || timeofday =="Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            else if(degrees>18 && degrees <= 24)
+            {
+                if(timeofday == "Morning")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if(timeofday == "Afternoon")
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if(timeofday == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            else if(degrees >= 25)
+            {
+                if(timeofday == "Morning")
+                {
+                    outfit = "T-shirt";
+                    shoes = "Sandals";
+                }
+                else if(timeofday == "Afternoon")
+                {
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+                else if (timeofday == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+
+
+        }
+    }
+}
