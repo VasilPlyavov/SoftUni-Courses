330b475cb5d914e6ea312ac7d000b66c10e87318
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-05-10 14:22:21 +0100

    Added Exercises

---
+++ Programming-Basics-C#/Exercises/02.If-Else Statement/Trade Commissions/Trade Commissions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Trade_Commissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            double sells = double.Parse(Console.ReadLine());
+            double commission = 0;
+            if (town == "Sofia")
+            {
+                if (sells >= 0 && sells <= 500) commission = 0.05;
+                else if (sells > 500 && sells <= 1000) commission = 0.07;
+                else if (sells > 1000 && sells <= 10000) commission = 0.08;
+                else if (sells > 10000) commission = 0.12;
+            }
+            else if (town == "Varna")
+            {
+                if (sells >= 0 && sells <= 500) commission = 0.045;
+                else if (sells > 500 && sells <= 1000) commission = 0.075;
+                else if (sells > 1000 && sells <= 10000) commission = 0.1;
+                else if (sells > 10000) commission = 0.13;
+            }
+            else if(town == "Plovdiv")
+            {
+                if (sells >= 0 && sells <= 500) commission = 0.055;
+                else if (sells > 500 && sells <= 1000) commission = 0.08;
+                else if (sells > 1000 && sells <= 10000) commission = 0.12;
+                else if (sells > 10000) commission = 0.145;
+            }
+
+            if (commission > 0)
+            {
+                commission = sells * commission;
+                Console.WriteLine($"{(commission):f2}");
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+
+
+
+        }
+    }
+}
