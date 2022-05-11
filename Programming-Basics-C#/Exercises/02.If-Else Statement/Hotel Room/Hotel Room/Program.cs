8448f826d404d7c83e4be5be897aca391dead8c8
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-05-11 14:44:39 +0100

    More Labs and Exercises

---
+++ Programming-Basics-C#/Exercises/02.If-Else Statement/Hotel Room/Hotel Room/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Hotel_Room
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string months = Console.ReadLine();
+            int nights = int.Parse(Console.ReadLine());
+            double studioPrice = 0;
+            double apartmentPrice = 0;
+
+
+            switch (months)
+            {
+                case "May":
+                case "October":
+                    if (nights <= 7)
+                    {
+                        studioPrice = nights * 50;
+                        apartmentPrice = nights * 65;
+                    }
+                    else if (nights > 7 && nights <= 12)
+                    {
+                        studioPrice = nights * 50 * 0.95;
+                        apartmentPrice = nights * 65;
+                    }
+                    else if (nights > 14)
+                    {
+                        studioPrice = nights * 50 * 0.70;
+                        apartmentPrice = nights * 65 * 0.90;
+                    }
+                    break;
+                case "June":
+                case "September":
+                    if (nights <= 14)
+                    {
+                        studioPrice = nights * 75.20;
+                        apartmentPrice = nights * 68.70;
+                    }
+                    else if (nights > 14)
+                    {
+                        studioPrice = nights * 75.20 * 0.80;
+                        apartmentPrice = nights * 68.70 * 0.90;
+                    }
+                    break;
+                case "July":
+                case "August":
+                    if (nights <= 14)
+                    {
+                        studioPrice = nights * 76 ;
+                        apartmentPrice = nights * 77 ;
+                    }
+                    else if (nights > 14)
+                    {
+                        studioPrice = nights * 76;
+                        apartmentPrice = nights * 77 * 0.90;
+                    }break;
+            }
+            Console.WriteLine($"Apartment: {apartmentPrice:f2} lv.");
+            Console.WriteLine($"Studio: {studioPrice:f2} lv.");
+        }
+    }
+}
