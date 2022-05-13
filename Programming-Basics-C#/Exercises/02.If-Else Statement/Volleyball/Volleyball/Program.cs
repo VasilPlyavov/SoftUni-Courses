daacda7d1673d550fe4e95529d877615094fa7e2
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-05-13 20:56:10 +0100

    Added  exercise

---
+++ Programming-Basics-C#/Exercises/02.If-Else Statement/Volleyball/Volleyball/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string year = Console.ReadLine();
+            double p = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double freeWeekendInSofia = (48 - h) * 3 / 4;
+            double numOfGames = freeWeekendInSofia + (p * 2 / 3) + h;
+            if(year == "leap")
+            {
+                numOfGames = numOfGames * 1.15;
+            }
+            Console.WriteLine($"{Math.Truncate(numOfGames)}");
+        }
+    }
+}
