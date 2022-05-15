5d1f587dd002ab303a39662ffb8666e888c70527
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-05-15 12:39:49 +0100

    Added Exercise

---
+++ Programming-Basics-C#/Exercises/03. For Loop/Back To The Past/Back To The Past/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Back_To_The_Past
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double heritageMoney = double.Parse(Console.ReadLine());
+            double yearToLeave = double.Parse(Console.ReadLine());
+            int years = 18;
+            
+
+            for (int currentYear =1800 ; currentYear <= yearToLeave; currentYear++)
+            {
+                if (currentYear % 2 == 0)
+                {
+                    heritageMoney -= 12000;
+                }
+                else
+                {
+                    heritageMoney -= (12000 + 50 * years);
+                }
+                years++;
+            }
+            if (heritageMoney >= 0)
+            {
+                Console.WriteLine($"Yes! He will live a carefree life and will have {heritageMoney:f2} dollars left.");
+            }
+            else
+            {
+                Console.WriteLine($"He will need {Math.Abs(heritageMoney):f2} dollars to survive.");
+            }
+           
+        }
+    }
+}
