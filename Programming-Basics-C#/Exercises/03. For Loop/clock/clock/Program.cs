958e5cff198723e2da2b20cd24a60f0c56082dfb
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-05-18 14:55:41 +0100

    Added exercises

---
+++ Programming-Basics-C#/Exercises/03. For Loop/clock/clock/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hour = 0; hour <=23; hour++)
+            {
+                for (int min = 0; min <=59; min++)
+                {
+                    for (int seconds = 0; seconds <=59; seconds++)
+                    {
+                        Console.WriteLine($"{hour} : {min} : {seconds}");
+                    }
+                   
+                }
+            }
+            
+        }
+    }
+}
