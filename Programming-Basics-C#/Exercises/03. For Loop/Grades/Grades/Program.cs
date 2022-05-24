7bcc537f0a5759830c03ae6c135f57179c4a6d1f
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-05-24 14:52:03 +0100

    Added Exercises

---
+++ Programming-Basics-C#/Exercises/03. For Loop/Grades/Grades/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double students = int.Parse(Console.ReadLine());
+            double morethan5 = 0;
+            double morethan4 = 0;
+            double morethan3 = 0;
+            double lessthan3 = 0;
+            double gradeResult = 0;
+            for (int i = 1; i <= students; i++)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                if (grade >= 5.00)
+                {
+                    morethan5++;
+                }
+                else if (grade >= 4.00)
+                {
+                    morethan4++;
+                }
+                else if (grade >= 3)
+                {
+                    morethan3++;
+                }
+                else 
+                {
+                    lessthan3++;
+                }
+                gradeResult += grade;
+            }
+            double topStudents = 100.0 * morethan5 / students;
+            double between4 = 100.0 * morethan4 / students;
+            double between3 = 100.0 * morethan3 / students;
+            double fail = 100.0 * lessthan3 / students;
+            double average = gradeResult / students;
+
+            Console.WriteLine($"Top students: {(topStudents):f2}%");
+            Console.WriteLine($"Between 4.00 and 4.99: {(between4):f2}%");
+            Console.WriteLine($"Between 3.00 and 3.99: {(between3):f2}%");
+            Console.WriteLine($"Fail: {(fail):f2}%");
+            Console.WriteLine($"Average: {(average):f2}");
+        }
+    }
+}
---
+++ Programming-Basics-C#/Exercises/03. For Loop/Hospital/Hospital/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int period = int.Parse(Console.ReadLine());
+            int treatmentPatients = 0;
+            int untreatmentPatients = 0;
+            int doctors = 7;
+
+            for (int day = 1; day <= period; day++)
+            {
+                int currentPatients = int.Parse(Console.ReadLine());
+                if(day%3==0 && (untreatmentPatients > treatmentPatients))
+                {
+                    doctors++;
+                }
+                if(currentPatients > doctors)
+                {
+                    treatmentPatients += doctors;
+                    untreatmentPatients += currentPatients - doctors;
+                }
+                else
+                {
+                    treatmentPatients += currentPatients;
+                      
+                }
+            }
+            Console.WriteLine($"Treated patients: {treatmentPatients}.");
+            Console.WriteLine($"Untreated patients: {untreatmentPatients}.");
+        }
+    }
+}
