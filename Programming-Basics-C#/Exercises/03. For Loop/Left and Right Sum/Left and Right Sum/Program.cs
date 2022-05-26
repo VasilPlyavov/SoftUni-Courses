7fc567fb72f3255121e46c2f044767ff6b9494b0
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-05-26 21:07:29 +0100

    Added labs

---
+++ Programming-Basics-C#/Exercises/03. For Loop/Left and Right Sum/Left and Right Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int leftsum = 0;
+            int rightsum = 0;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                leftsum += number;
+                sum = leftsum +rightsum;
+
+            }
+            for (int i = 0; i <n; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                rightsum += numbers;
+            }
+            if(leftsum == rightsum )
+            {
+                
+                Console.WriteLine("Yes, sum = " + leftsum);
+            }
+            else
+            {
+                double difference = Math.Abs(rightsum - leftsum);
+                Console.WriteLine("No, diff = " + difference);
+            }
+           
+        }
+    }
+}
