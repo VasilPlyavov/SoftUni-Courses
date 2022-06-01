6a4bb9fd8b1320ac65d7a1e0ecf5398e0b23a7e3
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-06-01 21:57:50 +0100

    Added exercises

---
+++ Programming-Basics-C#/Exercises/03. For Loop/Odd Even Sum/Odd Even Sum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int oddsum = 0;
+            int evensum = 0;
+          
+            for (int i = 0; i < n; i++)
+            {
+                int current = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evensum += current;
+                }
+                else if (i % 2 != 0)
+                {
+                    oddsum += current;
+
+                }
+
+            }
+            if (evensum == oddsum)
+            {
+                
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + evensum);
+            }
+            else
+            {
+                int difference = Math.Abs(oddsum - evensum);
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + difference);
+            }
+
+
+        }
+
+    }
+}
+
---
+++ Programming-Basics-C#/Exercises/03. For Loop/PC Game Shop/PC Game Shop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PC_Game_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soldGames = int.Parse(Console.ReadLine());
+            int hearthstoneCount = 0;
+            int forniteCount = 0;
+            int overwatchCount = 0;
+            int othersCount = 0;
+
+
+            for (int i = 0; i < soldGames; i++)
+            {
+                string name = Console.ReadLine();
+                switch (name)
+                {
+                    case "Hearthstone":hearthstoneCount++;break;
+                    case "Fornite":forniteCount++;break;
+                    case "Overwatch":overwatchCount++;break;
+                    default:othersCount++;break;
+
+                }
+            }
+            double resultHeart = 1.0 * hearthstoneCount / soldGames * 100;
+            double resultFornite = 1.0 * forniteCount / soldGames * 100;
+            double resultOverwatch = 1.0 * overwatchCount / soldGames * 100;
+            double resultOthers = 1.0 * othersCount / soldGames * 100;
+            Console.WriteLine($"Hearthstone - {resultHeart:f2}%");
+            Console.WriteLine($"Fornite - {resultFornite:f2}%");
+            Console.WriteLine($"Overwatch - {resultOverwatch:f2}%");
+            Console.WriteLine($"Others - {resultOthers:f2}%");
+        }
+    }
+}
