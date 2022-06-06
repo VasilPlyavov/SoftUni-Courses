46bca2b4c96937a667dd0b35515ff46434af33aa
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-06-06 15:01:26 +0100

    Added more exercises

---
+++ Programming-Basics-C#/Exercises/03. For Loop/Sum Numbers/Sum Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            double sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                int current = int.Parse(Console.ReadLine());
+                sum += current;
+            }
+            Console.WriteLine(sum);
+
+
+
+        }
+    }
+}
---
+++ Programming-Basics-C#/Exercises/03. For Loop/Tennis Ranklist/Tennis Ranklist/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tennis_Ranklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tournaments = int.Parse(Console.ReadLine());
+            int points = int.Parse(Console.ReadLine());
+
+            double averagesum = 0;
+            double wCount = 0;
+            double finalpoints = 0;
+            double totalaverage = 0;
+
+            for (int i = 1; i <= tournaments; i++)
+            {
+                string stage = Console.ReadLine();
+                switch (stage)
+                {
+                    case "W": points += 2000; finalpoints = 2000; wCount++; break;
+                    case "F": points += 1200; finalpoints = 1200; break;
+                    case "SF": points += 720; finalpoints = 720; break;
+
+                }
+                averagesum += finalpoints;
+                totalaverage = averagesum / tournaments;
+
+            }
+            Console.WriteLine($"Final points: {points}");
+            Console.WriteLine($"Average points: {Math.Floor(totalaverage)}");
+            Console.WriteLine($"{(1.0 * wCount / tournaments * 100):f2}% ");
+        }
+    }
+}
+
