3e197392483db56f9ef4a4ea54e61107e4324ee2
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-06-07 21:02:53 +0100

    Added more labs

---
+++ Programming-Basics-C#/Exercises/03. For Loop/Vowels Sum/Vowels Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            var result = 0;
+            for(int i = 0; i < text.Length; i++)
+            {
+                var sssss = text[i];
+                switch (sssss)
+                {
+                    case 'a': result += 1;break;
+                    case 'e': result += 2;break;
+                    case 'i': result += 3;break;
+                    case 'o': result += 4;break;
+                    case 'u': result += 5;break;
+                }
+                
+                Console.WriteLine(result);
+            }
+
+           
+        }
+    }
+}
