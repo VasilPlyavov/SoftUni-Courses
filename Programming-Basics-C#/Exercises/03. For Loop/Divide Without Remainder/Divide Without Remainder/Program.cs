6f48da86e477d78b751c4c41994f04f190304e27
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-06-08 20:48:21 +0100

    Added homework

---
+++ Programming-Basics-C#/Exercises/03. For Loop/Divide Without Remainder/Divide Without Remainder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Divide_Without_Remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+
+            for (int i = 0; i < n; i+=1)
+            {
+                double num = double.Parse(Console.ReadLine());
+                if (num % 2 == 0)
+                {
+                    p1 +=1;
+                }
+                if (num % 3 == 0)
+                {
+                    p2+=1;
+                }
+                if (num % 4 == 0)
+                {
+                    p3+=1;
+                }
+            }
+            Console.WriteLine($"{p1 / n * 100:f2}%");
+            Console.WriteLine($"{p2 / n * 100:f2}%");
+            Console.WriteLine($"{p3 / n * 100:f2}%");
+        }
+    }
+}
---
+++ Programming-Basics-C#/Exercises/03. For Loop/Half Sum Element/Half Sum Element/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            int sumwithoutMaxNumber = sum - max ;
+            if(max == sumwithoutMaxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + max);
+                
+            }
+            else
+            {
+                int difference = Math.Abs(max - sumwithoutMaxNumber);
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + difference);
+            }
+            
+        }
+    }
+}
