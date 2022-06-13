18fc8de649f0c3052dc99f20c123ad4d5d0054e7
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-06-13 21:02:29 +0100

    Added exercise

---
+++ Programming-Basics-C#/Exercises/03. For Loop/Salary/Salary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tab = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            bool isBancrupt = false;
+
+
+            for (int i = 0; i < tab; i++)
+            {
+                string name = Console.ReadLine();
+                switch (name)
+                {
+                    case "Facebook": salary -= 150; break;
+                    case "Instagram": salary -= 100; break;
+                    case "Reddit": salary -= 50; break;
+                }
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    isBancrupt = true;
+                    break;
+                }
+               
+                
+            }
+            if (!isBancrupt)
+            {
+                Console.WriteLine(salary);
+            }
+            
+        }
+    }
+}
