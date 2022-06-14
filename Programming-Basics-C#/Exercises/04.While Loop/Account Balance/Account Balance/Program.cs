cb8ace38ce10d357fbbaf959e1f2fabe4e14b99d
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-06-14 21:13:41 +0100

    Added homework

---
+++ Programming-Basics-C#/Exercises/04.While Loop/Account Balance/Account Balance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string input =(Console.ReadLine());
+            double balance = 0.0;
+
+            while(input != "NoMoreMoney")
+            {
+                double amount = double.Parse(input);
+                if(amount < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                balance += amount;
+                Console.WriteLine($"Increase: {amount:f2}");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {balance:f2}");
+        }
+    }
+}
---
+++ Programming-Basics-C#/Exercises/04.While Loop/Average Number/Average Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Average_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            for (int i = 0; i <n; i++)
+            {
+                sum+= double.Parse(Console.ReadLine());
+                
+            }
+            double average = sum * 1.0 / n;
+            Console.WriteLine($"{(average):f2}");
+        }
+    }
+}
