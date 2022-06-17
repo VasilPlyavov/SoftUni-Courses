d22d6c9d5f690f8b95531b4ceb1656e7f4ada229
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-06-17 22:24:59 +0100

    Added Labs exercises

---
+++ Programming-Basics-C#/Exercises/04.While Loop/Moving/Moving/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int hight = int.Parse(Console.ReadLine());
+
+            int volume = width * length * hight;
+            bool hasVolume = true;
+
+            string command = Console.ReadLine();
+
+            while (command != "Done")
+            { 
+                int box = int.Parse(command);
+                volume -= box;
+                if (volume < 0)
+                {
+                    hasVolume = false;
+                    break;
+                }
+                command = Console.ReadLine();
+            }
+            if (hasVolume)
+            {
+                Console.WriteLine($"{volume} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {Math.Abs(volume)} Cubic meters more.");
+            }
+        }
+    }
+}
