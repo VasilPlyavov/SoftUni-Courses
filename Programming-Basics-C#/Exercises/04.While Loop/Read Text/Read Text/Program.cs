ce90a247eb41ba9e5305c9903cd066cbfd00d625
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-06-20 14:36:58 +0100

    Added more Labs

---
+++ Programming-Basics-C#/Exercises/04.While Loop/Read Text/Read Text/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Read_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            while (name != "Stop")
+            {
+                name = Console.ReadLine();
+                
+            }
+
+            
+
+
+        }
+    }
+}
---
+++ Programming-Basics-C#/Exercises/04.While Loop/Report System/Report System/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Report_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalSum = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int payCash = 0;
+            int payCard = 0;
+            int cashCounter = 0;
+            int cardCounter = 0;
+            int paymentsCounter = 0;
+
+            while (input != "End")
+            {
+                paymentsCounter++;
+                int product = int.Parse(input);
+                if (paymentsCounter % 2 != 0 && product <= 100)
+                {
+                    cashCounter++;
+                    totalSum -= product;
+                    payCash += product;
+                    Console.WriteLine("Product sold!");
+                }
+                else if (paymentsCounter % 2 == 0 && product >= 100)
+                {
+                    cardCounter++;
+                    totalSum -= product;
+                    payCard += product;
+                    Console.WriteLine("Product sold!");
+                }
+                else
+                {
+                    Console.WriteLine("Error in transaction!");
+                }
+                if (totalSum <= 0)
+                {
+                    Console.WriteLine($"Average CS: {(1.0 * payCash / cashCounter):f2}");
+                    Console.WriteLine($"Average CC: {(1.0 * payCard / cardCounter):f2}");
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            if (input == "End")
+            {
+                Console.WriteLine("Failed to collect required money for charity.");
+            }
+            
+        }
+    }
+}
