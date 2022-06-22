2b899141e4047335df5d69184a9e1e0ec75e5d03
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-06-22 14:44:21 +0100

    Added exercises

---
+++ Programming-Basics-C#/Exercises/04.While Loop/Cake/Cake/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            bool hascake = true;
+            int totalpieces = width * length;
+
+            while (hascake)
+            {
+                string input = Console.ReadLine();
+                if (input == "STOP")
+                {
+                    break;
+                }
+                int pieces = int.Parse(input);
+                totalpieces -= pieces;
+                if (totalpieces < 0)
+                {
+                    hascake = false;
+                }
+            }
+            if (hascake)
+            {
+                Console.WriteLine($"{totalpieces} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(totalpieces)} pieces more.");
+            }
+
+        }
+    }
+}
---
+++ Programming-Basics-C#/Exercises/04.While Loop/Coins/Coins/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal change = decimal.Parse(Console.ReadLine());
+           
+            int coins = 0;
+            while (change > 0)
+            {
+                coins++;
+                if (change >= 2M)
+                {
+                    change -= 2;
+                }
+                else if(change >= 1M)
+                {
+                    change -= 1;
+                }
+                else if (change >= 0.50M)
+                {
+                    change -= 0.50M;
+                }
+                else if(change >=0.20M)
+                {
+                    change -= 0.20M;
+                }else if (change >= 0.10M)
+                {
+                    change -= 0.10M;
+                }else if (change >= 0.05M)
+                {
+                    change -= 0.05M;
+                }else if (change >= 0.02M)
+                {
+                    change -= 0.02M;
+                }
+                else if (change >= 0.01M)
+                {
+                    change -= 0.01M;
+                }
+            }
+            Console.WriteLine(coins);
+        }
+    }
+}
