88d062dec19f36ed1e59dfaabc0860372e3f494c
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-06-27 16:54:00 +0100

    Added exercises

---
+++ Programming-Basics-C#/Exercises/04.While Loop/Old Books/Old Books/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Old_Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string favouritebook = Console.ReadLine();
+            int counter = 0;
+            bool isBookFound = false;
+            string nextBookName = Console.ReadLine();
+
+            while (isBookFound= false)
+            {
+                nextBookName = Console.ReadLine();
+                counter++;
+                if (nextBookName == "No More Books")
+                {
+                   
+                    break;
+                }
+              if(nextBookName == favouritebook)
+                {
+                    isBookFound = true;
+                    break;
+                }
+                counter++;
+                nextBookName = Console.ReadLine();
+                if (isBookFound)
+                {
+                    Console.WriteLine($"You checked {counter-1} books and found it.");
+                    
+                }
+                else
+                {
+
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine($"You checked {counter-1} books.");
+                }
+            }
+            
+        }
+    }
+}
---
+++ Programming-Basics-C#/Exercises/04.While Loop/Old libriry/Old libriry/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Old_libriry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string favouriteBook = Console.ReadLine();
+            int bookCount = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+            bool found = false;
+
+            while (counter < bookCount)
+            {
+                string currentBookName = Console.ReadLine();
+
+                if (currentBookName == favouriteBook)
+                {
+                    found = true;
+                    break;
+                }
+                counter++;
+            }
+            if (found)
+            {
+                Console.WriteLine($"You checked {counter} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {counter} books.");
+
+            }
+        }
+    }
+}
