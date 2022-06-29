5a5a13da22f558d6e3cdcacc23e39b12499a0bd0
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-06-29 15:00:57 +0100

    Added exercises

---
+++ Programming-Basics-C#/Exercises/05.Nested Loops/Building/Building/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+
+            for (int i = floors; i>=1; i--)
+            {
+
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Programming-Basics-C#/Exercises/05.Nested Loops/Clock/Clock/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hour = 0; hour <=23; hour++)
+            {
+                for (int minutes = 0; minutes <=59; minutes++)
+                {
+                    Console.WriteLine($"{hour}:{minutes}");
+                }
+            }
+            
+        }
+    }
+}
