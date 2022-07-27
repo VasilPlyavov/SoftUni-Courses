1a43488562845c51232feb54798f923c4a590fbe
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-07-27 14:57:24 +0100

    Exam exercises

---
+++ Programming-Basics-C#/Exercises/06.Exam preparation/Easter Bake/Easter Bake/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Easter_Bake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sugarCount = 0;
+            double flourCount = 0;
+            double neededSugar = 0;
+            double neededFlour = 0;
+            double maxsugar = 0;
+            double maxflour = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                double sugarQuantity = double.Parse(Console.ReadLine());
+                double flourQuantity = double.Parse(Console.ReadLine());
+                neededSugar += sugarQuantity;//общо нужна захар
+                
+                neededFlour += flourQuantity;//общо нужно брашно
+               
+                if (sugarQuantity > maxsugar)
+                {
+                    maxsugar = sugarQuantity;
+                }
+                if (flourQuantity > maxflour)
+                {
+                    maxflour = flourQuantity;
+                }
+                sugarQuantity++;
+                flourQuantity++;
+            }
+            
+            double numPackageSugar = neededSugar / 950;
+            double numPackageFlour = neededFlour / 750;
+            Console.WriteLine($"Sugar: {Math.Ceiling(numPackageSugar)}");
+            Console.WriteLine($"Flour: {Math.Ceiling(numPackageFlour)}");
+            Console.WriteLine($"Max used flour is {maxflour} grams, max used sugar is {maxsugar} grams.");
+        }
+    }
+}
