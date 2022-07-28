b62376ffaa0cf8d16c5a1f02def2732c835831c9
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-07-28 14:52:54 +0100

    More exams exercises

---
+++ Programming-Basics-C#/Exercises/06.Exam preparation/Easter Lunch/Easter Lunch/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Easter_Lunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int redCount = 0;
+            int orangeCount = 0;
+            int blueCount = 0;
+            int greenCount = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                string color = (Console.ReadLine());
+                switch (color)
+                {
+                    case "red": redCount++; break;
+                    case "orange": orangeCount++; break;
+                    case "blue": blueCount++; break;
+                    case "green": greenCount++; break;
+
+                }
+
+            }
+            int maxEggs = redCount;
+            string maxCollor = "red";
+            if (orangeCount > maxEggs)
+            {
+                maxEggs = orangeCount;
+                maxCollor = "red";
+            }
+            if (blueCount > maxEggs)
+            {
+                maxEggs = blueCount;
+                maxCollor = "blue";
+            }
+            if (greenCount > maxEggs)
+            {
+                maxEggs = greenCount;
+                maxCollor = "green";
+            }
+            Console.WriteLine($"Red eggs: {redCount}");
+            Console.WriteLine($"Orange eggs: {orangeCount}");
+            Console.WriteLine($"Blue eggs: {blueCount}");
+            Console.WriteLine($"Green eggs: {greenCount}");
+            Console.WriteLine($"Max eggs: {maxEggs} -> {maxCollor}");
+        }
+    }
+}
---
+++ Programming-Basics-C#/Exercises/06.Exam preparation/Fitness Center/Fitness Center/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Fitness_Center
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ("Back", "Chest", 'Legs", "Abs"
+            //"Protein shake" или "Protein bar
+            int numVisitors = int.Parse(Console.ReadLine());
+            int backCounter = 0;
+            int chestCounter = 0;
+            int legsCounter = 0;
+            int absCounter = 0;
+            int proteinshakeCounter = 0;
+            int proteinbarCounter = 0;
+
+            for (int i = 0; i < numVisitors; i++)
+            {
+                string type = Console.ReadLine();
+                switch (type)
+                {
+                    case "Back": backCounter++; break;
+                    case "Chest": chestCounter++; break;
+                    case "Legs": legsCounter++; break;
+                    case "Abs": absCounter++; break;
+                    case "Protein shake": proteinshakeCounter++; break;
+                    case "Protein bar": proteinbarCounter++; break;
+                }
+                
+            }
+            int totaltrained = backCounter + chestCounter + legsCounter + absCounter;//pehat trenirashti
+            double trainedPeople = 1.0 * totaltrained / numVisitors * 100;//pechat 60%
+            double proteinPeople = proteinshakeCounter + proteinbarCounter;//hora kupili protein
+            double percentageproteinPeople = 1.0 * proteinPeople / numVisitors * 100;//kupili protein
+
+            Console.WriteLine($"{backCounter} - back");
+            Console.WriteLine($"{chestCounter} - chest");
+            Console.WriteLine($"{legsCounter} - legs");
+            Console.WriteLine($"{absCounter} - abs");
+            Console.WriteLine($"{proteinshakeCounter} - protein shake");
+            Console.WriteLine($"{proteinbarCounter} - protein bar");
+            Console.WriteLine($"{(trainedPeople):f2}% - work out");
+            Console.WriteLine($"{(percentageproteinPeople):f2}% - protein");
+
+        }
+    }
+}
