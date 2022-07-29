224dd8659c70856289463561e611c6345bd5000c
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-07-29 22:47:24 +0100

    Added more exercises

---
+++ Programming-Basics-C#/Exercises/06.Exam preparation/Movie Ratings/Movie Ratings/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Movie_Ratings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string name = "";
+            double raiting = 0;
+            double minRaiting = double.MaxValue;
+            string movieMaxRaiting = "";
+            double maxRaiting = double.MinValue;
+            string movieMinRaiting = "";
+            double average = 0;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                name = Console.ReadLine();
+                raiting = double.Parse(Console.ReadLine());
+
+                if (raiting > maxRaiting)
+                {
+                    maxRaiting = raiting;
+                    movieMaxRaiting = name;
+                }
+                if (raiting < minRaiting)
+                {
+                    minRaiting = raiting;
+                    movieMinRaiting = name;
+                }
+                average += raiting;
+            }
+            Console.WriteLine($"{movieMaxRaiting} is with highest rating: {maxRaiting:f1}");
+            Console.WriteLine($"{movieMinRaiting} is with lowest rating: {minRaiting:f1}");
+            Console.WriteLine($"Average rating: {average / n:f1}");
+        }
+    }
+}
---
+++ Programming-Basics-C#/Exercises/06.Exam preparation/Series/Series/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int numSerial = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+            double price = 0;
+            string name = "";
+            for (int i = 1; i <= numSerial; i++)
+            {
+                name = Console.ReadLine();
+                price = double.Parse(Console.ReadLine());
+
+                if (name == "Thrones")
+                {
+                    price = price * 0.50;
+
+                }
+                if (name == "Lucifer")
+                {
+                    price = price * 0.60;
+
+                }
+                if (name == "Protector")
+                {
+                    price = price * 0.70;
+
+                }
+                if (name == "TotalDrama")
+                {
+                    price = price * 0.80;
+
+                }
+                if (name == "Area")
+                {
+                    price = price * 0.90;
+
+                }
+                else
+                {
+                    price = price;
+
+                }
+
+                totalPrice += price;
+
+            }
+            if (budget >= totalPrice)
+            {
+                Console.WriteLine($"You bought all the series and left with {(budget - totalPrice):f2} lv.");
+            }
+            else
+            {
+                Console.WriteLine($"You need {Math.Abs(budget - totalPrice):f2} lv. more to buy the series!");
+            }
+
+        }
+    }
+}
