6abfecc1a5aec5d083acb91f26b70188d97ede62
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-07-31 13:12:30 +0100

    Added exercises from new course

---
+++ Fundamentals/Exercises/01.5.BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/Back in 30 minutes/Back in 30 minutes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Back_in_30_minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hour = double.Parse(Console.ReadLine());
+            double minutes = double.Parse(Console.ReadLine()) + 30 ;
+
+            if (hour >= 23)
+            {
+                hour = 0;
+                minutes = minutes - 60;
+                
+            }
+            if (minutes > 59)
+            {
+               
+                hour++;
+                minutes -= 60;
+            }
+            
+            if (minutes <= 9)
+            {
+                Console.WriteLine($"{hour}:0{minutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{hour}:{minutes}");
+            }
+            
+        }
+    }
+}
