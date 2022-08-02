887cf83dfe1a75a743ec3f9c8c7908efbd6ca087
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-08-02 14:59:55 +0100

    Added more exercises

---
+++ Fundamentals/Exercises/01.5.BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/Even Number/Even Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= number; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (currentNumber % 2 == 0)
+                {
+                    Console.WriteLine($"The number is: {currentNumber}");
+                }
+                else 
+                {
+                    Console.WriteLine($"Please write an even number.");
+                }
+
+            }
+
+
+        }
+
+    }
+}
+
