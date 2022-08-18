b56cbb41c761b20dbf45e361d40e263dfbb40e3d
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-08-18 14:50:46 +0100

    Added homework

---
+++ Fundamentals/Exercises/02.6.EXERCISE BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/10.Padawan Equipment/Padawan Equipment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Padawan_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amountOfMoney = double.Parse(Console.ReadLine());
+            int studentsCounts = int.Parse(Console.ReadLine());
+            double priceOfLightsabers = double.Parse(Console.ReadLine());
+            double priceOfRobes = double.Parse(Console.ReadLine());
+            double priceOfBelts = double.Parse(Console.ReadLine());
+
+            double totalSum = priceOfLightsabers * Math.Ceiling(studentsCounts * 1.1) +
+            priceOfRobes * studentsCounts +
+            priceOfBelts * (studentsCounts - (studentsCounts / 6));
+
+
+            if (amountOfMoney>=totalSum)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalSum:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"John will need {(totalSum-amountOfMoney):F2}lv more.");
+            }
+           
+
+        }
+    }
+}
