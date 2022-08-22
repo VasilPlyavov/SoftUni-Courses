6169dd3f543d76fdfd8e4ab0fc8be6cec3c12d7b
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-08-22 20:57:32 +0100

    Added exercises

---
+++ Fundamentals/Exercises/03.7.DATA TYPES AND VARIABLES/Chars to String/Chars to String/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char oneSymbol = char.Parse(Console.ReadLine());
+            char secondSymbol = char.Parse(Console.ReadLine());
+            char thirdSymbol = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{oneSymbol}{secondSymbol}{thirdSymbol}");
+        }
+    }
+}
