a2efc2724367bc3e9e042321b260b8dcad960f15
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-08-24 14:49:32 +0100

    Added homework

---
+++ Fundamentals/Exercises/03.7.DATA TYPES AND VARIABLES/Concat Names/Concat Names/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Concat_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string secondName = Console.ReadLine();
+            string delitel = Console.ReadLine();
+
+            Console.WriteLine($"{firstName}{delitel}{secondName}");
+        }
+    }
+}
---
+++ Fundamentals/Exercises/03.7.DATA TYPES AND VARIABLES/Convert Meters to Kilometers/Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal result = n / 1000.0M;
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
