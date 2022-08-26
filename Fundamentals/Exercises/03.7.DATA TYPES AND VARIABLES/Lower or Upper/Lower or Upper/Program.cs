900f989d28da94cf97dcf97f080b2cea8129aae0
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-08-26 20:54:09 +0100

    Added more exercises

---
+++ Fundamentals/Exercises/03.7.DATA TYPES AND VARIABLES/Lower or Upper/Lower or Upper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string example = Console.ReadLine();
+            string uppercase = example.ToUpper();
+            if (example == uppercase)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+
+        }
+    }
+}
---
+++ Fundamentals/Exercises/03.7.DATA TYPES AND VARIABLES/Pounds to Dollars/Pounds to Dollars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number = decimal.Parse(Console.ReadLine());
+            decimal pound = number * 1.31M;
+            
+            Console.WriteLine($"{pound:f3}");
+        }
+    }
+}
