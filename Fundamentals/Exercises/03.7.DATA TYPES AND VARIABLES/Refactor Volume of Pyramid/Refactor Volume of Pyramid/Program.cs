1d5f555dad8dcde897fe1564f1a7bc9e83ab49d0
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-08-30 21:08:29 +0100

    Added exercises

---
+++ Fundamentals/Exercises/03.7.DATA TYPES AND VARIABLES/Refactor Volume of Pyramid/Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Refactor_Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double v =0;
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double heigth = double.Parse(Console.ReadLine());
+            v = (length + width + heigth) / 3;
+            Console.WriteLine($"Length:{length}");
+           
+            Console.WriteLine($"Width:{width}");
+            
+            Console.WriteLine($"Heigth:{heigth}");
+           
+            Console.WriteLine($"Pyramid Volume: {v:f2}");
+        }
+    }
+}
