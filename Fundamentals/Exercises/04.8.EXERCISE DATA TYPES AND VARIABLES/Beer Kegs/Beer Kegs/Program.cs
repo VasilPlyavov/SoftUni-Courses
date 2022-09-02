e51d71443855bd747866befd23bb1d41e38ceb61
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-09-02 14:25:51 +0100

    Added exercise

---
+++ Fundamentals/Exercises/04.8.EXERCISE DATA TYPES AND VARIABLES/Beer Kegs/Beer Kegs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            float maxVolume = float.MinValue;
+            string biggestModel = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                float volume = (float)(Math.PI * Math.Pow(radius, 2) * height);
+                if (volume > maxVolume)
+                {
+                    maxVolume = volume;
+                    biggestModel = model;
+                }
+            }
+            Console.WriteLine(biggestModel);
+        }
+    }
+}
