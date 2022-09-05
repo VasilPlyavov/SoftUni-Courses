d8bce1edf0a9cfc3fca6dc38ed726db7b543793c
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-09-05 21:00:55 +0100

    Added exercises

---
+++ Fundamentals/Exercises/04.8.EXERCISE DATA TYPES AND VARIABLES/Elevator/Elevator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfPEople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int courses = (int)Math.Ceiling(numOfPEople / (double)capacity);
+            Console.WriteLine(courses);
+        }
+    }
+}
---
+++ Fundamentals/Exercises/04.8.EXERCISE DATA TYPES AND VARIABLES/Integer Operations/Integer Operations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            int secondSum = firstNumber + secondNumber;
+            int thirdSum = secondSum / thirdNumber;
+            int fourtSum = thirdSum * fourthNumber;
+            Console.WriteLine(fourtSum);
+        }
+    }
+}
