a77d6576e18ee58145be4195b11a622a13dd502c
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-09-06 20:59:39 +0100

    Added homework

---
+++ Fundamentals/Exercises/04.8.EXERCISE DATA TYPES AND VARIABLES/Poke Mon/Poke Mon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            byte y = byte.Parse(Console.ReadLine());
+
+            int targetCount = 0;
+            int originalValue = n;
+
+
+            while (n >= m)
+            {
+                if(n==originalValue/2 && y>0)
+                {
+                    
+                        n /= y;
+
+                    if (n < m)
+                    {
+                        break;
+                    }
+                }
+                n -= m;
+                targetCount++;
+            }
+            Console.WriteLine(n);
+            Console.WriteLine(targetCount);
+        }
+    }
+}
