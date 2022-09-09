0797ee258e82244065440b392675077c80328786
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-09-09 14:52:47 +0100

    Added more exercise

---
+++ Fundamentals/Exercises/04.8.EXERCISE DATA TYPES AND VARIABLES/Sum Digits/Sum Digits/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                sum += int.Parse(num[i].ToString());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
