d28fa977e6f373df2c026792a6d66538d853dd5b
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-09-13 14:22:32 +0100

    Added exercise

---
+++ Fundamentals/Exercises/04.8.EXERCISE DATA TYPES AND VARIABLES/Water Overflow/Water Overflow/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int totalLitres = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int litres = int.Parse(Console.ReadLine());
+                if (totalLitres + litres > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    totalLitres += litres;
+
+                }
+            }
+            Console.WriteLine($"{totalLitres}");
+        }
+    }
+}
