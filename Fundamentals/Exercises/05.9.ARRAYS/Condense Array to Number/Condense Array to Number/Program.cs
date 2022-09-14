da90553067f159009d640f9613ef982ae8ac0997
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-09-14 14:20:34 +0100

    Added exercise

---
+++ Fundamentals/Exercises/05.9.ARRAYS/Condense Array to Number/Condense Array to Number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = Console.ReadLine()
+                  .Split()
+                  .Select(int.Parse)
+                  .ToArray();
+
+            if (nums.Length==1)
+            {
+                Console.WriteLine(nums[0]);
+                return;
+            }
+            int[] condense = new int[nums.Length - 1];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < condense.Length-i; j++)
+                {
+
+                    condense[j] = nums[j] + nums[j + 1];
+
+                }
+                nums = condense;
+            }
+            Console.WriteLine(condense[0]);
+        }
+    }
+}
