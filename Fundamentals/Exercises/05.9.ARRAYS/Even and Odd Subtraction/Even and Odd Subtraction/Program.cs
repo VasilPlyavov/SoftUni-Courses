29016c221eb689b1c64ec576f01684226b52fbe4
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-09-15 20:50:59 +0100

    Added homework

---
+++ Fundamentals/Exercises/05.9.ARRAYS/Even and Odd Subtraction/Even and Odd Subtraction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumbers = numbers[i];
+                if (currentNumbers % 2 == 0)
+                {
+                    evenSum += currentNumbers;
+                }
+                else
+                {
+                    oddSum += currentNumbers;
+                }
+            }
+
+            
+            Console.WriteLine($"{evenSum - oddSum}");
+        }
+    }
+}
