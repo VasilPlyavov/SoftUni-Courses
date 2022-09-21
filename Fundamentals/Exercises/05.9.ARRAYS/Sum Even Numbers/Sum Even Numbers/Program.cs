30354986d854d92ea659b30f593e2d5fb4508650
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-09-21 20:58:48 +0100

    Added more exercise

---
+++ Fundamentals/Exercises/05.9.ARRAYS/Sum Even Numbers/Sum Even Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+namespace Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+
+            {
+                int currentNumbers = numbers[i];
+                if (currentNumbers % 2 == 0)
+                {
+                    sum += currentNumbers;
+                }
+
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
