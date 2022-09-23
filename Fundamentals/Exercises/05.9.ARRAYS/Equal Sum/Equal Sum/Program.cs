1b9aac55d2b19b97fbbc234ac773387564d6575f
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-09-23 21:08:17 +0100

    Added exercises

---
+++ Fundamentals/Exercises/05.9.ARRAYS/Equal Sum/Equal Sum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+namespace Equal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int leftSum = 0;
+                int rigthSum = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += array[j];
+                }
+                for (int k = i + 1; k < array.Length; k++)
+                {
+                    rigthSum += array[k];
+                }
+                if (leftSum == rigthSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
