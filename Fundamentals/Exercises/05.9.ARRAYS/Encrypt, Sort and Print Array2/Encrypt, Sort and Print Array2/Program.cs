0ca7e08f13fb3255d3361df5f4bdaabe353a30fa
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-10-04 20:43:17 +0100

    Added homework

---
+++ Fundamentals/Exercises/05.9.ARRAYS/Encrypt, Sort and Print Array2/Encrypt, Sort and Print Array2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Encrypt__Sort_and_Print_Array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int[] sums = new int[numbers];
+            for (int i = 0; i < numbers; i++)
+            {
+                string inputString = Console.ReadLine();
+                sums[i] = sumVowelsConsonants(inputString);
+            }
+            Array.Sort(sums);
+            foreach (var item in sums)
+            {
+                Console.WriteLine(item);
+            }
+        }
+         static int sumVowelsConsonants(string text)
+        {
+            int sum = 0;
+            foreach (char element in text)
+            {
+                if (element=='a' || element=='A' || element=='e'|| element=='E' ||
+                    element=='i'|| element=='I' || element=='u' || element=='U'
+                    || element=='o' || element=='O')
+                {
+                    sum += (int)element * text.Length;
+                }
+                else
+                {
+                    sum += (int)element / text.Length;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Fundamentals/Exercises/05.9.ARRAYS/Fold and Sum/Fold and Sum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int[] array1 = new int[array.Length / 2];
+            int[] array2 = new int[array.Length / 2];
+            int n = 0;
+            for (int i = array.Length/4-1; i >= 0; i--)
+            {
+                array1[n] = array[i];
+                n++;
+            }
+            n = array.Length / 2 - 1;
+                for (int i = array.Length-array.Length/4; i <array.Length ; i++)
+            {
+                array1[n] = array[i];
+                n--;
+            }
+            n = 0;
+            int start = array.Length / 4;
+            int end = array.Length - array.Length / 4;
+            for (int i = start; i < end; i++)
+            {
+                array2[n] = array[i];
+                n++;
+            }
+            int[] sum = new int[array1.Length];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                sum[i] = array1[i] + array2[i];
+            }
+            Console.WriteLine(string.Join(" ", sum));
+        }
+    }
+}
