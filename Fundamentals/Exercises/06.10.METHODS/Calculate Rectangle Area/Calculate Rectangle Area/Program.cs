0b3d69ba2927ce9120d5e9df94214324f0c7978a
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-10-06 21:12:12 +0100

    Added exercises

---
+++ Fundamentals/Exercises/06.10.METHODS/Calculate Rectangle Area/Calculate Rectangle Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double heigth = double.Parse(Console.ReadLine());
+            double area = CalcRectangleArea(width, heigth);
+            Console.WriteLine(area);
+        }
+        static double CalcRectangleArea(double width, double heigth)
+        {
+            return width* heigth;
+        }
+    }
+}
---
+++ Fundamentals/Exercises/06.10.METHODS/Calculations/Calculations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Calculations
+{
+    class Program
+    {
+        static void Main()
+        {
+            string command = Console.ReadLine();
+
+            var firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            switch (command)
+            {
+                case "add": Add(firstNum,secondNum); break;
+                case "multiply": Multiply(firstNum,secondNum); break;
+                case "subtract": Subtract(firstNum,secondNum); break;
+                case "divide": Divide(firstNum,secondNum); break;
+            }
+        }
+        static void Add(int firstNum,  int secondNum)
+        {
+            int result = firstNum + secondNum;
+            Console.WriteLine($"{result}");
+
+        }
+        static void Multiply(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum * secondNum);
+        }
+        static void Subtract(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum - secondNum);
+        }
+        static void Divide(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum / secondNum);
+        }
+    }
+}
