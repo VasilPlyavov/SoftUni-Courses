d9130399845c0085d567db73a5e1e445c869a416
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-10-07 21:04:16 +0100

    Added exercises

---
+++ Fundamentals/Exercises/06.10.METHODS/Greater of Two Values/Greater of Two Values/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Greater_of_Two_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            if (input == "int")
+            {
+                int firstnum = int.Parse(Console.ReadLine());
+                int secondnum = int.Parse(Console.ReadLine());
+                int output = GetIntMax(firstnum, secondnum);
+                Console.WriteLine(output);
+            }
+            else if (input == "char")
+            {
+                char firstnum = char.Parse(Console.ReadLine());
+                char secondnum = char.Parse(Console.ReadLine());
+                char output = GetCharMax(firstnum, secondnum);
+                Console.WriteLine(output);
+            }
+            else if (input == "string")
+            {
+                string firstnum = Console.ReadLine();
+                string secondnum = Console.ReadLine();
+                string output = GetStringMax(firstnum, secondnum);
+                Console.WriteLine(output);
+            }
+
+        }
+        static int GetIntMax(int firstnum, int secondnum)
+        {
+            if (firstnum >= secondnum)
+            {
+                return firstnum;
+            }
+            else
+            {
+                return secondnum;
+            }
+        }
+        static char GetCharMax(char firstnum, char secondnum)
+        {
+            if (firstnum >= secondnum)
+            {
+                return firstnum;
+            }
+            else
+            {
+                return secondnum;
+            }
+        }
+        static string GetStringMax(string firstnum, string secondnum)
+        {
+            
+            string theBiggest = string.Empty;
+            if (firstnum.CompareTo(secondnum) >= 0)
+            {
+                theBiggest = firstnum;
+                return theBiggest;
+            }
+            else
+            {
+                theBiggest = secondnum;
+                return theBiggest;
+            }
+
+
+        }
+    }
+}
