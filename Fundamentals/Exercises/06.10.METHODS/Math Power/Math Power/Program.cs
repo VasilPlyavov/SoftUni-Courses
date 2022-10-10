58891d39fbc9fd7b053ba58b44de63dde728f9b6
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-10-10 20:46:15 +0100

    Added exercises

---
+++ Fundamentals/Exercises/06.10.METHODS/Math Power/Math Power/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double result = MakePower(number, power);
+            Console.WriteLine(result);
+        }
+
+        static double MakePower(double number, int power)
+        {
+            double powerNumber = 1;
+
+            for (int i = 1; i <= power; i++)
+            {
+                powerNumber *= number;
+            }
+            return powerNumber;
+
+        }
+        
+    }
+}
---
+++ Fundamentals/Exercises/06.10.METHODS/Math operations/Math operations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            int second = int.Parse(Console.ReadLine());
+            double result = Calculations(first, @operator, second);
+            Console.WriteLine(result);
+        }
+        static double Calculations(int first, string @operator, int second)
+        {
+            double result = 0;
+            switch(@operator)
+            {
+                case "/":result = first / second;break;
+                case "*":result = first * second;break;
+                case "+":result = first + second;break;
+                case "-":result = first - second;break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
