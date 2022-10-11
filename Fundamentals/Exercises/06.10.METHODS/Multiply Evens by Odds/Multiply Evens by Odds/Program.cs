f1576d2d03dbdfc64b2ed65260ee23c6da95a8d6
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-10-11 13:33:34 +0100

    Added more exercises

---
+++ Fundamentals/Exercises/06.10.METHODS/Multiply Evens by Odds/Multiply Evens by Odds/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Multiply_Evens_by_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Math.Abs(int.Parse(Console.ReadLine()));
+            int evenSum = GetSumOfEvenDigits(input);
+            int oddSum = GetSumOfOddDigits(input);
+            int result = GetMultipleOfEvenAndOdd(evenSum,oddSum);
+            Console.WriteLine(result);
+        }
+        static int GetSumOfEvenDigits(int input)
+        {
+            int evenSum = 0;
+            while (input > 0)
+            {
+                if (input % 2 == 0)
+                {
+                    evenSum += input % 10;
+                }
+                input /= 10;
+            }
+            return evenSum;
+        }
+        static int GetSumOfOddDigits(int input)
+        {
+            int oddSum = 0;
+            while (input > 0)
+            {
+                if (input % 2 != 0)
+                {
+                    oddSum += input % 10;
+                }
+                input /= 10;
+            }
+            return oddSum;
+        }
+        private static int GetMultipleOfEvenAndOdd(int evenSum, int oddSum)
+        {
+            return evenSum * oddSum;
+        }
+    }
+}
---
+++ Fundamentals/Exercises/06.10.METHODS/Orders/Orders/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string products = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            //coffee, coke, water, snacks;
+            PrintSum(products,quantity);
+        }
+        static void PrintSum(string products, int quantity)
+        {
+            
+            double totalSum = 0;
+            switch (products)
+            {
+                case "coffee": totalSum += quantity * 1.50; break;
+                case "coke": totalSum += quantity * 1.40; break;
+                case "water":totalSum += quantity * 1.00;break;
+                case "snacks":totalSum += quantity * 2.00;break;
+                default:
+                    break;
+            }
+            Console.WriteLine($"{totalSum:f2}");
+        }
+    }
+}
