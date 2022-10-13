1149e6b233588437a28939bf6579183f4f814a8a
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-10-13 21:14:18 +0100

    Added exercise

---
+++ Fundamentals/Exercises/06.10.METHODS/Repeat String/Repeat String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+namespace Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            RepeatString(str, count);
+        }
+        private static void RepeatString(string str, int count)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                result.Append(str);
+                
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
