2c82045a1510fbf261b59c905efdc17cb500dc76
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-10-18 21:03:52 +0100

    Added more exercises

---
+++ Fundamentals/Exercises/06.10.METHODS/Center Point/Center Point/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Center_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double xA = double.Parse(Console.ReadLine());
+            double yA = double.Parse(Console.ReadLine());
+            double xB = double.Parse(Console.ReadLine());
+            double yB = double.Parse(Console.ReadLine());
+
+            PrintClosestPointToCenter(xA, yA, xB, yB);
+        }
+
+        static void PrintClosestPointToCenter(double xA, double yA, double xB, double yB)
+        {
+            double distanceA = Math.Sqrt((xA * xA) + (yA * yA)) / 2;
+            double distanceB = Math.Sqrt((xB * xB) + (yB * yB)) / 2;
+
+            if (distanceA <= distanceB)
+            {
+                Console.WriteLine($"({xA}, {yA})");
+            }
+            else
+            {
+                Console.WriteLine($"({xB}, {yB})");
+            }
+
+        }
+            
+    }
+}
---
+++ Fundamentals/Exercises/06.10.METHODS/Data Type Finder2/Data Type Finder2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Data_Type_Finder2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string final = String.Empty;
+
+            switch (input)
+            {
+                case "int":
+                    int intInput = int.Parse(Console.ReadLine());
+                    final = InputOperation(intInput);
+                    break;
+                case "real":
+                    double doubleInput = double.Parse(Console.ReadLine());
+                    final = $"{DoubleOperation(doubleInput):f2}";
+                    break;
+                case "string":
+                    string strInput = Console.ReadLine();
+                    final = InputOperation(strInput);
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine(final);
+        }
+
+         static string InputOperation(string strInput)
+        {
+            return $"${strInput}$";
+        }
+
+         static double DoubleOperation(double doubleInput)
+        {
+            return doubleInput * 1.5;
+        }
+
+        private static string InputOperation(int input)
+        {
+            return (input * 2).ToString();
+        }
+    }
+}
