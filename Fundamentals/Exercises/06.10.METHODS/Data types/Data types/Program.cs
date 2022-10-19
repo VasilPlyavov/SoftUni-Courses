06baaf8fdbadf1701236ae33bbe726ec1d07ea32
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-10-19 21:04:12 +0100

    Added homework

---
+++ Fundamentals/Exercises/06.10.METHODS/Data types/Data types/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            if (type=="int")
+            {
+                int num = int.Parse(Console.ReadLine());
+                intMethod(num);
+            }
+            else if (type=="real")
+            {
+                double num = double.Parse(Console.ReadLine());
+                doubleMethod(num);
+            }
+            else if (type=="string")
+            {
+                string word= Console.ReadLine();
+                stringWord(word);
+            }
+        }
+
+        static void stringWord(string word)
+        {
+            Console.WriteLine("$" + word + "$");
+        }
+
+        static void doubleMethod(double num)
+        {
+            Console.WriteLine($"{(num*1.5):f2}");
+        }
+
+        static void intMethod(int num)
+        {
+            Console.WriteLine(num * 2);
+        }
+    }
+}
+
---
+++ Fundamentals/Exercises/06.10.METHODS/Longer Line/Longer Line/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Longer_Line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
+
+            LongestLine(x1, y1, x2, y2, x3, y3, x4, y4);
+        }
+
+        static void LongestLine(double a, double b, double c, double d, double e, double f, double g, double k)
+        {
+            double firstLine = Math.Sqrt(Math.Pow(Math.Abs(a - c), 2) + Math.Pow(Math.Abs(b - d), 2));
+            double secondLine = Math.Sqrt(Math.Pow(Math.Abs(e - g), 2) + Math.Pow(Math.Abs(f - k), 2));
+
+            if (firstLine >= secondLine)
+            {
+                ClosesToZero(a, b, c, d);
+            }
+            else
+            {
+                ClosesToZero(e, f, g, k);
+            }
+
+        }
+
+        static void ClosesToZero(double a, double b, double c, double d)
+        {
+            double first = Math.Sqrt(Math.Pow(Math.Abs(a), 2) + Math.Pow(Math.Abs(b), 2));
+            double second = Math.Sqrt(Math.Pow(Math.Abs(c), 2) + Math.Pow(Math.Abs(d), 2));
+
+            if (first > second)
+            {
+                Console.WriteLine($"({c}, {d})({a}, {b})");
+            }
+            else
+            {
+                Console.WriteLine($"({a}, {b})({c}, {d})");
+            }       
+
+        }
+    }
+}
