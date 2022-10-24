00860f0510a13e3c13fdd59f83fa3d065462c5c1
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-10-24 21:10:19 +0100

    Added exercises

---
+++ Fundamentals/Exercises/06.10.METHODS/Add and Subtract/Add and Subtract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int result = AddSum(first, second, third) - SubstractSum(first, second, third); ;
+            Console.WriteLine(result);
+
+        }
+        static int AddSum(int first, int second,int third)
+        {
+            int result = 0;
+            result = first + second;
+            return result;
+        }
+        static int SubstractSum(int first, int second, int third)
+        {
+            int result = 0;
+             result = third - result;
+            return result;
+        }
+    }
+}
