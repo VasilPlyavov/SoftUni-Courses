9fc4e445eab3e7688508dd4382149580c36ec1f6
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-10-25 21:03:25 +0100

    Added homework

---
+++ Fundamentals/Exercises/06.10.METHODS/Factorial Division/Factorial Division/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstFactorial = int.Parse(Console.ReadLine());
+            int secondFactorial = int.Parse(Console.ReadLine());
+            double factorialFirst = GetFactorial(firstFactorial);
+            double factorialSecond = GetFactorial(secondFactorial);
+            double result = factorialFirst / factorialSecond;
+            Console.WriteLine($"{result:f2}");
+        }
+
+        private static double GetFactorial(int first)
+        {
+            double result = 1;
+            while (first != 1)
+            {
+                result = result * first;
+                first = first - 1;
+            }
+            return result;
+        }
+    }
+}
---
+++ Fundamentals/Exercises/06.10.METHODS/Middle Characters/Middle Characters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PrintMiddleChar(input);
+        }
+        static void PrintMiddleChar(string input)
+        {
+            char[] charString = input.ToCharArray();
+            int charCounter = 0;
+            int center = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                charCounter++;
+            }
+            if (charCounter % 2 == 0)
+            {
+                center = input.Length / 2;
+                Console.WriteLine($"{charString[center - 1]}{charString[center]}");
+            }
+            else
+            {
+                center = (input.Length + 1) / 2;
+                Console.WriteLine($"{charString[center - 1]}");
+            }
+        }
+    }
+}
