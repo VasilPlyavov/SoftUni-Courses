85fa09f06eb6c6f2215f03d5c29f9a0e07729be2
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-10-31 20:43:00 +0000

    Added exercises

---
+++ Fundamentals/Exercises/06.10.METHODS/Vowels Count/Vowels Count/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+            ReturnVowelsCount(input);
+
+        }
+        static void ReturnVowelsCount(string input)
+        {
+            int vowelCounter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case 'a': vowelCounter++; break;
+                    case 'e': vowelCounter++; break;
+                    case 'o': vowelCounter++; break;
+                    case 'i': vowelCounter++; break;
+                    case 'u': vowelCounter++; break;
+                    default: break;
+                }
+            }
+            Console.WriteLine(vowelCounter);
+
+        }
+
+    }
+}
