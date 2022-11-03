01f0a6ddd304ac900e448ab1c961089b6ecba62b
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-11-03 20:56:22 +0000

    Added exercises

---
+++ Fundamentals/Exercises/07.11.LISTS/List of Products/List of Products/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string currentProducts = Console.ReadLine();
+                products.Add(currentProducts);
+            }
+            products.Sort();
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
