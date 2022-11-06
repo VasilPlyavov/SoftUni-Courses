6210f26c35f97286f4bd5da229a6a88f3e84bd69
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-11-06 14:09:38 +0000

    Added homework

---
+++ Fundamentals/Exercises/07.11.LISTS/Append Arrays/Append Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine().Split("|").ToList();
+
+
+
+            items.Reverse();//obryshtane na masiva
+
+            List<string> result = new List<string>();
+
+            foreach (string currentItems in items)
+            {
+                string[] numbers = currentItems.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                foreach (string numbersToAdd  in numbers )
+                {
+                    result.Add(numbersToAdd);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
