a0dc65c4d51396047bacb0574059ae36f7ab0f5d
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-11-17 21:00:52 +0000

    Added homework

---
+++ Fundamentals/Exercises/08.12.OBJECTS AND CLASSES/Students/Students/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Students
+{
+    class Student
+    {
+         public string FirstName { get; set; }
+
+         public string LastName { get; set; }
+         public int Age { get; set; }
+         public string City { get; set; }
+
+    }
+}
