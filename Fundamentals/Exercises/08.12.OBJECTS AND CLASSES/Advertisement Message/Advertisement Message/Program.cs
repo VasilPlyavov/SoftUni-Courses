0736cc1eb783367c7b901f42e598c3e3021c97e6
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-11-21 21:11:16 +0000

    Added exercises

---
+++ Fundamentals/Exercises/08.12.OBJECTS AND CLASSES/Advertisement Message/Advertisement Message/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Advertisement_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] phrases = { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product." };
+            string[] events = { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+            string[] autors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+            for (int i = 0; i <n; i++)
+            {
+                Random rnd = new Random();
+                string message = phrases[rnd.Next(0, phrases.Length)];
+                string place = events[rnd.Next(0, events.Length)];
+                string author = autors[rnd.Next(0, autors.Length)];
+                string city = cities[rnd.Next(0, cities.Length)];
+                Console.WriteLine($"{message} {place} {author} – {city}.");
+            }
+        }
+    }
+}
---
+++ Fundamentals/Exercises/08.12.OBJECTS AND CLASSES/Articles/Articles/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Articles
+{
+    class Program
+    {
+        static void Main(string[] args)//razpisvame logikata v Main methoda;
+        {
+            string[] tokens = Console.ReadLine().Split(", ");
+            Article article = new Article(tokens[0],tokens[1],tokens[2]);
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArg = Console.ReadLine().Split(": ");//vzimame input
+                string command = cmdArg[0];
+                string argument = cmdArg[1];
+                switch (command)
+                {
+                    case "Edit":article.Edit(argument);break;//izvikvame methoda
+                    case "ChangeAuthor": article.ChangeAuthor(argument);break;
+                    case "Rename":article.Rename(argument);break;
+
+                }
+            }
+            Console.WriteLine(article.ToString());
+        }
+
+    }
+    class Article
+    {
+        public Article(string title,string content,string author)//constructor
+        {
+            Title = title;//parameters
+            Content = content;
+            Author = author;
+
+        }
+        public string Title { get; set; }//properties
+        public string Content { get; set; }//properties
+        public string Author { get; set; }//properties
+
+
+        public void Edit(string content)//Methods
+        {
+            Content = content;//change content ot uslovieto;
+        }
+
+        public void ChangeAuthor(string author)//Methods
+        {
+            Author = author;//
+        }
+        public void Rename(string title)//Methods
+        {
+            Title = title;//preimenuva naimeto;
+        }
+        public override string ToString()//Methods
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
