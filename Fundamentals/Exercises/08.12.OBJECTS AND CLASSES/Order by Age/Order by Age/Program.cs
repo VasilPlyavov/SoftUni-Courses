9e792cee2708ebe23fb3b4cc63cff703e2ef3d8d
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-11-22 20:25:20 +0000

    Added more exercises

---
+++ Fundamentals/Exercises/08.12.OBJECTS AND CLASSES/Order by Age/Order by Age/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Order_by_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] inputInfo = input.Split(" ");
+                string name = inputInfo[0];
+                string id = inputInfo[1];
+                int age = int.Parse(inputInfo[2]);
+                Person person = new Person(name, id, age);
+                people.Add(person);
+
+
+                input = Console.ReadLine();
+            }
+            var orderedAge = people.OrderBy(x => x.Age);
+
+            foreach (Person person in orderedAge)
+            {
+                Console.WriteLine(person.Name + " with ID: " +  person.ID + " is " + person.Age + " years old.");
+            }
+        }
+        class Person
+        {
+
+            public Person(string name, string iD, int age)
+            {
+                Name = name;
+                ID = iD;
+                Age = age;
+            }
+
+            public string Name { get; set; }
+            public string ID { get; set; }
+            public int Age { get; set; }
+
+
+        }
+    }
+
+
+
+}
+
+
