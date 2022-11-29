371f61c257b0e8c9f496e264bfc464e9e7f69d7a
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-11-29 20:14:44 +0000

    Added more exercises

---
+++ Fundamentals/Exercises/09.13.ASSOCIATIVE ARRAYS/Odd Occurrences/Odd Occurrences/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!counts.ContainsKey(words[i]))
+                {
+                    counts.Add(words[i], 1);
+                    continue;
+                }
+
+                counts[words[i]]++;
+            }
+            Console.WriteLine(string.Join(" ", counts.Where(v => v.Value % 2 != 0).Select(v => v.Key)));
+        }
+
+
+
+    }
+
+}
+    
+
