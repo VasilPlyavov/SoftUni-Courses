005238d3986d6b7f4c70b4f51c5b7287a2cec39c
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-12-05 20:34:31 +0000

    Added homework

---
+++ Fundamentals/Exercises/09.13.ASSOCIATIVE ARRAYS/A Miner Task/A Miner Task/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            string resource = Console.ReadLine();
+
+            while (resource != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                if (!resources.ContainsKey(resource))
+                {
+                    resources.Add(resource, quantity);
+                    resource = Console.ReadLine();
+                    continue;
+                }
+                resources[resource] += quantity;
+                resource = Console.ReadLine();
+
+                
+            }
+            foreach (var res in resources)
+            {
+                Console.WriteLine($"{res.Key} -> {res.Value}");
+            }
+        }
+    }
+}
