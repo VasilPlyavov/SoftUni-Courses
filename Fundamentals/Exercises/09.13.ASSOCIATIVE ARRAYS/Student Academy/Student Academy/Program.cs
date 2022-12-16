dac3d2c9d5d8cdd315213151dcc61665ad28ab1e
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-12-16 20:45:26 +0000

    Added more exercise

---
+++ Fundamentals/Exercises/09.13.ASSOCIATIVE ARRAYS/Student Academy/Student Academy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> studentData = new Dictionary<string, List<double>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!studentData.ContainsKey(studentName))
+                {
+                    studentData.Add(studentName, new List<double>());
+                    studentData[studentName].Add(grade);
+                    continue;
+                }
+                studentData[studentName].Add(grade);
+            }
+            studentData = studentData.Where(s => s.Value.Average()
+              >= 4.50).ToDictionary(s => s.Key, s => s.Value);
+            foreach ( var student in studentData.OrderByDescending(s=>s.Value.Average()))
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+            } 
+        }
+    }
+}
