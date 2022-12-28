fd3b646471d85eec83b483f7987d028c8bf3289b
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-12-28 19:33:56 +0000

    Added exercises

---
+++ Fundamentals/Exercises/10.14.TEXT PROCESSING/Caesar Cipher/Caesar Cipher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder toCypher1 = new StringBuilder();
+            toCypher1.Append(Console.ReadLine());
+            StringBuilder cypher = new StringBuilder();
+            foreach (var @char in toCypher1.ToString())
+            {
+                cypher.Append((char)(@char + 3));
+            }
+            Console.WriteLine(cypher);
+        }
+    }
+}
---
+++ Fundamentals/Exercises/10.14.TEXT PROCESSING/Character Multiplier/Character Multiplier/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Character_Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strings = Console.ReadLine().Split().ToList();
+            List<char> shorter = strings[0].Length < strings[1].Length
+                ? strings[0].ToCharArray().ToList() : strings[1].ToCharArray().ToList();
+            List<char> longer = strings[0] == strings[1] ? shorter.ToList() :
+                      strings.First(s => s != string.Join("", shorter)).ToCharArray().ToList();
+            int sum = 0;
+            while (shorter.Count!=0)
+            {
+                sum += shorter[0] * longer[0];
+                shorter.Remove(shorter[0]);
+                longer.Remove(longer[0]);
+
+            }
+            if (longer.Count>0)
+            {
+                foreach (var @char in longer)
+                {
+                    sum += @char;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
