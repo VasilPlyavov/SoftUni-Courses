3a6b120975d4556a688c363e9ec99586b6e2e999
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-12-29 20:45:01 +0000

    Added homework

---
+++ Fundamentals/Exercises/10.14.TEXT PROCESSING/Extract File/Extract File/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder filePath = new StringBuilder();
+            filePath.Append(Console.ReadLine());
+            int indexLastSlash = filePath.ToString().LastIndexOf('\\');
+            filePath.Remove(0, indexLastSlash + 1);
+            int indexPoint = filePath.ToString().IndexOf('.');
+            Console.WriteLine($"File name: {filePath.ToString().Substring(0, indexPoint)}" +
+                $"\nFile extension: {filePath.ToString().Substring(indexPoint + 1)}");
+
+        }
+    }
+}
