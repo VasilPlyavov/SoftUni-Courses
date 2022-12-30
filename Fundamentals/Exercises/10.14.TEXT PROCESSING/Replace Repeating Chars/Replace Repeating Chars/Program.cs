5f803f46962317ffa0a5394b0359eb355169a1fb
Vasil Plyavov <vasil.plyavov@gmail.com>
2022-12-30 21:51:38 +0000

    Added more exercises

---
+++ Fundamentals/Exercises/10.14.TEXT PROCESSING/Replace Repeating Chars/Replace Repeating Chars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Console.ReadLine());
+            for (int i = 1; i < sb.Length; i++)
+            {
+                if (sb[i-1] ==sb[i])
+                {
+                    sb.Remove(i - 1, 1);
+                    i--;
+                }
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
