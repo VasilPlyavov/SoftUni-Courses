c8feece1e8b246e11605662715a9e7124bbd9a53
Vasil Plyavov <vasil.plyavov@gmail.com>
2023-01-01 22:01:02 +0000

    Added exercise

---
+++ Fundamentals/Exercises/10.14.TEXT PROCESSING/Valid Usernames/Valid Usernames/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+            foreach (var username in usernames)
+            {
+                if (Validation(username))
+                {
+                    Console.WriteLine(username);
+
+                }
+
+            }
+
+        }
+        static bool Validation(string valid)
+        {
+            bool isValid = true;
+            foreach (var @char in valid)
+            {
+                if ((char.IsLetterOrDigit(@char) || @char == '_' || @char == '-') && valid.Length >= 3 && valid.Length <= 16)
+                    {
+                    isValid = true;
+                }
+                else
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+            return isValid;
+        }
+    }
+}
